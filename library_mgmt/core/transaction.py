ca42062155485c77ba0af2e95dcf77006fd985c2
NiharikaRaoD <niharika.dharmana@outlook.com>
2025-10-08 21:57:19 -0500

    added additional modules

---
+++ library_mgmt/core/transaction.py
@@ -0,0 +1,41 @@
+""" This module defines Transaction class for library borrows and returns """
+
+from core.assets import LibraryAsset
+from core.users import Person
+
+
+class Transaction:
+    """Represents a borrowing or returning transaction in the library system
+
+    Attributes:
+        person (Person): The person performing the transaction
+        item (LibraryAsset): The asset involved in the transaction
+    """
+
+    def __init__(self, person: Person, item: LibraryAsset):
+        """Initializes a Transaction instance.
+
+        Args:
+            person (Person): The person performing the transaction
+            item (LibraryAsset): The asset being borrowed or returned
+        """
+        self._person = person
+        self._item = item
+
+    @property
+    def person(self) -> Person:
+        """Returns the person involved in the transaction
+
+        Returns:
+            Person: The person who borrowed or returned the item
+        """
+        return self._person
+    
+    @property
+    def item(self) -> LibraryAsset:
+        """Returns the item involved in the transaction
+
+        Returns:
+            LibraryAsset: The borrowed or returned asset
+        """
+        return self._item
